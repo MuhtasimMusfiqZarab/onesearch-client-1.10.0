f19986884ba45f72860bd35d356b2daca9b114f3
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-03-19 17:43:24 +0600

    adding sidebar

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,36 +1,133 @@
 import React, { Component } from 'react';
+import { Card, CardBody, CardTitle, Container, Row, Col } from 'reactstrap';
 import Link from 'next/link';
 
 class Header extends Component {
   state = {};
   render() {
     return (
-      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 className="my-0 mr-md-auto font-weight-normal">
-          <Link href="/">
-            <a>OneSearch</a>
-          </Link>
-        </h5>
-        <nav className="my-2 my-md-0 mr-md-3p-2 text-dark">
-          <Link href="/dashboard">
-            <a className="p-2 text-dark">Dashboard</a>
-          </Link>
-          <a className="p-2 text-dark" href="#">
-            Features
-          </a>
-          <a className="p-2 text-dark" href="#">
-            Enterprise
-          </a>
-          <a className="p-2 text-dark" href="#">
-            Support
-          </a>
-          <a className="p-2 text-dark" href="#">
-            Pricing
-          </a>
-        </nav>
-        <a className="btn btn-outline-primary" href="#">
-          Sign up
-        </a>
+      <div className="header bg-gradient-dark pb-8 pt-5 pt-md-8">
+        <Container fluid>
+          <div className="header-body">
+            {/* Card stats */}
+            <Row>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Traffic
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">
+                          350,897
+                        </span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i className="fas fa-chart-bar" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                        <i className="fa fa-arrow-up" /> 3.48%
+                      </span>{' '}
+                      <span className="text-nowrap">Since last month</span>
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          New users
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">2,356</span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
+                          <i className="fas fa-chart-pie" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-danger mr-2">
+                        <i className="fas fa-arrow-down" /> 3.48%
+                      </span>{' '}
+                      <span className="text-nowrap">Since last week</span>
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Sales
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">924</span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
+                          <i className="fas fa-users" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-warning mr-2">
+                        <i className="fas fa-arrow-down" /> 1.10%
+                      </span>{' '}
+                      <span className="text-nowrap">Since yesterday</span>
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col lg="6" xl="3">
+                <Card className="card-stats mb-4 mb-xl-0">
+                  <CardBody>
+                    <Row>
+                      <div className="col">
+                        <CardTitle
+                          tag="h5"
+                          className="text-uppercase text-muted mb-0"
+                        >
+                          Performance
+                        </CardTitle>
+                        <span className="h2 font-weight-bold mb-0">49,65%</span>
+                      </div>
+                      <Col className="col-auto">
+                        <div className="icon icon-shape bg-info text-white rounded-circle shadow">
+                          <i className="fas fa-percent" />
+                        </div>
+                      </Col>
+                    </Row>
+                    <p className="mt-3 mb-0 text-muted text-sm">
+                      <span className="text-success mr-2">
+                        <i className="fas fa-arrow-up" /> 12%
+                      </span>{' '}
+                      <span className="text-nowrap">Since last month</span>
+                    </p>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Container>
       </div>
     );
   }
--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,261 +1,256 @@
-import React, { Component } from 'react';
+/*eslint-disable*/
+import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-class SideBar extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="sidebar-sticky pt-3">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <a className="nav-link active" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-home"
-              >
-                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                <polyline points="9 22 9 12 15 12 15 22"></polyline>
-              </svg>
-              Dashboard <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-file"
-              >
-                <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
-                <polyline points="13 2 13 9 20 9"></polyline>
-              </svg>
-              Orders
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-shopping-cart"
-              >
-                <circle cx="9" cy="21" r="1"></circle>
-                <circle cx="20" cy="21" r="1"></circle>
-                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-              </svg>
-              Products
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-users"
-              >
-                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                <circle cx="9" cy="7" r="4"></circle>
-                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-              </svg>
-              Customers
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-bar-chart-2"
-              >
-                <line x1="18" y1="20" x2="18" y2="10"></line>
-                <line x1="12" y1="20" x2="12" y2="4"></line>
-                <line x1="6" y1="20" x2="6" y2="14"></line>
-              </svg>
-              Reports
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-layers"
-              >
-                <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-                <polyline points="2 17 12 22 22 17"></polyline>
-                <polyline points="2 12 12 17 22 12"></polyline>
-              </svg>
-              Integrations
-            </a>
-          </li>
-        </ul>
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Collapse,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
 
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
-          <a
-            className="d-flex align-items-center text-muted"
-            href="#"
-            aria-label="Add a new report"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              className="feather feather-plus-circle"
+var ps;
+
+function SideBar(props) {
+  // used for checking current route
+  const router = useRouter();
+  const [collapseOpen, setCollapseOpen] = React.useState(false);
+  // verifies if routeName is the one active (in browser input)
+  const activeRoute = (routeName) => {
+    return router.route.indexOf(routeName) > -1;
+  };
+  // toggles collapse between opened and closed (true/false)
+  const toggleCollapse = () => {
+    setCollapseOpen(!collapseOpen);
+  };
+  // closes the collapse
+  const closeCollapse = () => {
+    setCollapseOpen(false);
+  };
+  // creates the links that appear in the left menu / Sidebar
+  const createLinks = (routes) => {
+    return routes.map((prop, key) => {
+      return (
+        <NavItem key={key} active={activeRoute(prop.layout + prop.path)}>
+          <Link href={prop.layout + prop.path}>
+            <NavLink
+              href="#pablo"
+              active={activeRoute(prop.layout + prop.path)}
+              onClick={closeCollapse}
             >
-              <circle cx="12" cy="12" r="10"></circle>
-              <line x1="12" y1="8" x2="12" y2="16"></line>
-              <line x1="8" y1="12" x2="16" y2="12"></line>
-            </svg>
+              <i className={prop.icon} />
+              {prop.name}
+            </NavLink>
+          </Link>
+        </NavItem>
+      );
+    });
+  };
+  const { routes, logo } = props;
+  let navbarBrand = (
+    <NavbarBrand href="#pablo" className="pt-0">
+      <img alt={logo.imgAlt} className="navbar-brand-img" src={logo.imgSrc} />
+    </NavbarBrand>
+  );
+  return (
+    <Navbar
+      className="navbar-vertical fixed-left navbar-light bg-white"
+      expand="md"
+      id="sidenav-main"
+    >
+      <Container fluid>
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleCollapse}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        {/* Brand */}
+        {logo && logo.innerLink ? (
+          <Link href={logo.innerLink}>
+            <span>{navbarBrand}</span>
+          </Link>
+        ) : null}
+        {logo && logo.outterLink ? (
+          <a href={logo.innerLink} target="_blank">
+            {navbarBrand}
           </a>
-        </h6>
-        <ul className="nav flex-column mb-2">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-file-text"
-              >
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                <polyline points="14 2 14 8 20 8"></polyline>
-                <line x1="16" y1="13" x2="8" y2="13"></line>
-                <line x1="16" y1="17" x2="8" y2="17"></line>
-                <polyline points="10 9 9 9 8 9"></polyline>
-              </svg>
-              Current month
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-file-text"
-              >
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                <polyline points="14 2 14 8 20 8"></polyline>
-                <line x1="16" y1="13" x2="8" y2="13"></line>
-                <line x1="16" y1="17" x2="8" y2="17"></line>
-                <polyline points="10 9 9 9 8 9"></polyline>
-              </svg>
-              Last quarter
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-file-text"
-              >
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                <polyline points="14 2 14 8 20 8"></polyline>
-                <line x1="16" y1="13" x2="8" y2="13"></line>
-                <line x1="16" y1="17" x2="8" y2="17"></line>
-                <polyline points="10 9 9 9 8 9"></polyline>
-              </svg>
-              Social engagement
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-file-text"
-              >
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                <polyline points="14 2 14 8 20 8"></polyline>
-                <line x1="16" y1="13" x2="8" y2="13"></line>
-                <line x1="16" y1="17" x2="8" y2="17"></line>
-                <polyline points="10 9 9 9 8 9"></polyline>
-              </svg>
-              Year-end sale
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+        ) : null}
+        {/* User */}
+        <Nav className="align-items-center d-md-none">
+          <UncontrolledDropdown nav>
+            <DropdownToggle nav className="nav-link-icon">
+              <i className="ni ni-bell-55" />
+            </DropdownToggle>
+            <DropdownMenu
+              aria-labelledby="navbar-default_dropdown_1"
+              className="dropdown-menu-arrow"
+              right
+            >
+              <DropdownItem>Action</DropdownItem>
+              <DropdownItem>Another action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Something else here</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          <UncontrolledDropdown nav>
+            <DropdownToggle nav>
+              <Media className="align-items-center">
+                <span className="avatar avatar-sm rounded-circle">
+                  <img
+                    alt="..."
+                    src={require('assets/img/theme/team-1-800x800.jpg')}
+                  />
+                </span>
+              </Media>
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" right>
+              <DropdownItem className="noti-title" header tag="div">
+                <h6 className="text-overflow m-0">Welcome!</h6>
+              </DropdownItem>
+              <Link href="/admin/profile">
+                <DropdownItem>
+                  <i className="ni ni-single-02" />
+                  <span>My profile</span>
+                </DropdownItem>
+              </Link>
+              <Link href="/admin/profile">
+                <DropdownItem>
+                  <i className="ni ni-settings-gear-65" />
+                  <span>Settings</span>
+                </DropdownItem>
+              </Link>
+              <Link href="/admin/profile">
+                <DropdownItem>
+                  <i className="ni ni-calendar-grid-58" />
+                  <span>Activity</span>
+                </DropdownItem>
+              </Link>
+              <Link href="/admin/profile">
+                <DropdownItem>
+                  <i className="ni ni-support-16" />
+                  <span>Support</span>
+                </DropdownItem>
+              </Link>
+              <DropdownItem divider />
+              <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
+                <i className="ni ni-user-run" />
+                <span>Logout</span>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+        {/* Collapse */}
+        <Collapse navbar isOpen={collapseOpen}>
+          {/* Collapse header */}
+          <div className="navbar-collapse-header d-md-none">
+            <Row>
+              {logo ? (
+                <Col className="collapse-brand" xs="6">
+                  {logo.innerLink ? (
+                    <Link href={logo.innerLink}>
+                      <img alt={logo.imgAlt} src={logo.imgSrc} />
+                    </Link>
+                  ) : (
+                    <a href={logo.outterLink}>
+                      <img alt={logo.imgAlt} src={logo.imgSrc} />
+                    </a>
+                  )}
+                </Col>
+              ) : null}
+              <Col className="collapse-close" xs="6">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  onClick={toggleCollapse}
+                >
+                  <span />
+                  <span />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          {/* Form */}
+          <Form className="mt-4 mb-3 d-md-none">
+            <InputGroup className="input-group-rounded input-group-merge">
+              <Input
+                aria-label="Search"
+                className="form-control-rounded form-control-prepended"
+                placeholder="Search"
+                type="search"
+              />
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>
+                  <span className="fa fa-search" />
+                </InputGroupText>
+              </InputGroupAddon>
+            </InputGroup>
+          </Form>
+          {/* Navigation */}
+          <Nav navbar>{createLinks(routes)}</Nav>
+          {/* Divider */}
+          <hr className="my-3" />
+          {/* Heading */}
+          <h6 className="navbar-heading text-muted">Documentation</h6>
+          {/* Navigation */}
+          <Nav className="mb-md-3" navbar>
+            <NavItem>
+              <NavLink href="https://www.creative-tim.com/learning-lab/nextjs/overview/argon-dashboard?ref=njsad-admin-sidebar">
+                <i className="ni ni-spaceship" />
+                Getting started
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.creative-tim.com/learning-lab/nextjs/colors/argon-dashboard?ref=njsad-admin-sidebar">
+                <i className="ni ni-palette" />
+                Foundation
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.creative-tim.com/learning-lab/nextjs/avatar/argon-dashboard?ref=njsad-admin-sidebar">
+                <i className="ni ni-ui-04" />
+                Components
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <Nav className="mb-md-3" navbar>
+            <NavItem className="active-pro active">
+              <NavLink href="https://www.creative-tim.com/product/argon-dashboard-pro-react?ref=njsad-admin-sidebar">
+                <i className="ni ni-spaceship" />
+                Upgrade to PRO
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default SideBar;
