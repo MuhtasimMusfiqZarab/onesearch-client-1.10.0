712ea97fc090592971145694c6eb24cd30f435fe
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-05-12 18:35:15 +0600

    add sidebar

--- components/layouts/BaseLayout.tsx
+++ components/layouts/BaseLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TopNav from '../shared/topnav';
+import SideNav from '../shared/sidenav';
 
 const BaseLayout = (props) => {
   return (
     <React.Fragment>
       <TopNav />
+      <SideNav />
       {props.children}
     </React.Fragment>
   );
