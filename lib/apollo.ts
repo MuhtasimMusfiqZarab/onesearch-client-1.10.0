f947bca0516d190ba1accb7f78d5c4b44e28a48f
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-05-12 07:35:36 +0600

    test heroku deploy

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,5 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+console.log('This is the endpoint', process.env.NEXT_PUBLIC_API_SERVER);
+console.log('This is the secret', process.env.secret);
+
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_SERVER,
   cache: new InMemoryCache(),
