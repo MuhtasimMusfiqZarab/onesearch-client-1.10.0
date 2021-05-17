5721620eadf2ada839837f541c091134ed6f56c3
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-05-18 02:35:23 +0600

    add heroku backend

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://161.97.97.183:5000/graphql',
+  // uri: 'http://161.97.97.183:5000/graphql',
+  uri: 'https://onesearchio.herokuapp.com/graphql',
   cache: new InMemoryCache(),
 });
