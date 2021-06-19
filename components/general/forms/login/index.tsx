af92effdcd753c323292240a5aff8e753c1c3cae
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-06-19 11:21:47 +0600

    create a basic login page

--- components/general/forms/login/index.tsx
+++ components/general/forms/login/index.tsx
@@ -1,28 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 import {
   YoutubeIcon,
   FacebookIcon,
   GoogleMapIcon,
-  SettingsIcon,
-  ExpandIcon,
+  SigninIcon,
+  SignupIcon,
 } from 'components/_icons';
-
-import Image from 'next/image';
-import styles from './style.module.scss';
 
 export interface Props {}
 
 const Login: FC<Props> = (): JSX.Element => {
+  const [isSignUpMode, setIsSignupMode] = useState<boolean>(false);
+
   return (
     <>
-      <div className="login-container">
+      <div className={`login-container  ${isSignUpMode ? 'signup-mode' : ''} `}>
         <div className="forms-container">
           <div className="signin-signup">
-            <form action="" className="signin-form">
-              <h2 className="title">sign in</h2>
+            <form action="" className="sign-in-form">
+              <h2 className="title">Sign In</h2>
               <div className="input-field">
                 <div className="icon">
                   <FontAwesomeIcon icon={faUser} />
@@ -53,6 +52,87 @@ const Login: FC<Props> = (): JSX.Element => {
                 </a>
               </div>
             </form>
+            <form action="" className="sign-up-form">
+              <h2 className="title">Sign Up</h2>
+              <div className="input-field">
+                <div className="icon">
+                  <FontAwesomeIcon icon={faUser} />
+                </div>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <div className="icon">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </div>
+                <input type="text" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <div className="icon">
+                  <FontAwesomeIcon icon={faLock} />
+                </div>
+
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="text" value="Sign up" className="btn btn-solid" />
+              <p className="social-text">Or sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <FacebookIcon />
+                </a>
+                <a href="#" className="social-icon">
+                  <YoutubeIcon />
+                </a>
+                <a href="#" className="social-icon">
+                  <GoogleMapIcon />
+                </a>
+                <a href="#" className="social-icon">
+                  <GoogleMapIcon />
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New Here?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deleniti accusamus libero exercitationem neque, impedit
+                consequatur, quidem consectetur numquam molestiae ratione veniam
+                fugit? Dolore perspiciatis officia deserunt quibusdam
+                repudiandae, iusto voluptate!
+              </p>
+              <button
+                className="btn transparent"
+                id="sign-up-button"
+                onClick={() => setIsSignupMode(true)}
+              >
+                Sign Up
+              </button>
+            </div>
+            <img src="/sign.svg" alt="" className="image" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>Have an account?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deleniti accusamus libero exercitationem neque, impedit
+                consequatur, quidem consectetur numquam molestiae ratione veniam
+                fugit? Dolore perspiciatis officia deserunt quibusdam
+                repudiandae, iusto voluptate!
+              </p>
+              <button
+                className="btn transparent"
+                id="sign-in-button"
+                onClick={() => setIsSignupMode(false)}
+              >
+                Sign In
+              </button>
+            </div>
+
+            <img src="/sign.svg" alt="" className="image" />
           </div>
         </div>
       </div>
