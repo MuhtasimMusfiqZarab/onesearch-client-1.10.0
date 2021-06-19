7040de187a4397011afe3cf5f53f3a3f9ec03af2
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-06-19 17:44:47 +0600

    adding routes for each page

--- components/top-nav/index.tsx
+++ components/top-nav/index.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '../_icons';
+import Link from 'next/link';
 import styles from './style.module.scss';
 
 export default function TopNav() {
@@ -20,20 +21,23 @@ export default function TopNav() {
       <ul className={styles.nav__menu}>
         <li>
           <a href="#">
-            <svg viewBox="0 0 384 512" width="100">
-              <path d="M368 32h-56a16 16 0 0 0-16 16v48h-48V48a16 16 0 0 0-16-16h-80a16 16 0 0 0-16 16v48H88.1V48a16 16 0 0 0-16-16H16A16 16 0 0 0 0 48v176l64 32c0 48.33-1.54 95-13.21 160h282.42C321.54 351 320 303.72 320 256l64-32V48a16 16 0 0 0-16-16zM224 320h-64v-64a32 32 0 0 1 64 0zm144 128H16a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h352a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z" />
-            </svg>
-            {/* <Logo /> */}
+            <img src="/planet.svg" alt="" className="image" />
           </a>
         </li>
         <li>
-          <a href="#">About</a>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
         <li>
-          <a href="#">Work</a>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
         </li>
         <li>
-          <a href="#">Contact</a>
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
         </li>
       </ul>
     </nav>
