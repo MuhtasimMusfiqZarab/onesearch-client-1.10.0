6c3282ee81f9fd439b4d4de1fd2c8b4eec386b28
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-06-24 13:42:08 +0600

    remove table style from main style

--- components/table/index.tsx
+++ components/table/index.tsx
@@ -7,24 +7,24 @@ export interface Props {
 export default function Table({ items }: Props) {
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Channel Name</th>
-            <th>Joined</th>
-            <th>Subscribers</th>
-            <th>Views</th>
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr className={styles.tr}>
+            <th className={styles.th}>Channel Name</th>
+            <th className={styles.th}>Joined</th>
+            <th className={styles.th}>Subscribers</th>
+            <th className={styles.th}>Views</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className={styles.tbody}>
           {items.map((item, index) => (
-            <tr key={index}>
-              <td>
+            <tr key={index} className={styles.tr}>
+              <td className={styles.td}>
                 <a href="#">{item.channel_name}</a>
               </td>
-              <td>{item.joined}</td>
-              <td>{item.subscribers}</td>
-              <td>{item.views}</td>
+              <td className={styles.td}>{item.joined}</td>
+              <td className={styles.td}>{item.subscribers}</td>
+              <td className={styles.td}>{item.views}</td>
             </tr>
           ))}
         </tbody>
