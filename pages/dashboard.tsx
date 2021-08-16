5a43f870033614af1e36f134659af74f703aebdf
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-08-16 22:01:55 +0600

    relocate youtube table

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import BaseLayout from 'components/layouts/BaseLayout';
-import YoutubeTable from 'components/youtube-table';
-import { ChannelsProvider } from 'components/_context/channels';
-import { CountriesProvider } from 'components/_context/countries';
+import Head from "next/head";
+import BaseLayout from "components/layouts/BaseLayout";
+import YoutubeTable from "components/pages/dashboard/search/youtube";
+import { ChannelsProvider } from "components/_context/channels";
+import { CountriesProvider } from "components/_context/countries";
 
 export default function Index() {
   return (
