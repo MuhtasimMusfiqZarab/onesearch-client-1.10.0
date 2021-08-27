9b1f2d18662f703fb6f7c6d8612dad9902d265a4
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-08-27 14:41:25 +0600

    layout rename

--- pages/dashboard/search/index.tsx
+++ pages/dashboard/search/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import BaseLayout from "components/layouts/BaseLayout";
+import BaseLayout from "components/layouts/dashboard";
 import YoutubeTable from "components/pages/dashboard/search/youtube";
 import { ChannelsProvider } from "components/_context/youtube/channels";
 import { CountriesProvider } from "components/_context/youtube/countries";
