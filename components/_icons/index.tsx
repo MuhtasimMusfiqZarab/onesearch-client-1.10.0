a754ef344c62d6146c0824dea15f567fb102dc9f
ashrafuzzaman93 <skshahid.im@gmail.com>
2021-08-28 16:44:50 +0600

    shahid/os-38/table-layout-for-mobile

--- components/_icons/index.tsx
+++ components/_icons/index.tsx
@@ -17,3 +17,7 @@ export { NotFoundIcon } from "./notFound";
 export { ServerErrorIcon } from "./serverError";
 export { BreadIcon } from "./bread";
 export { CloseIcon } from "./close";
+export { Save } from "./save";
+export { DownArrow } from "./downArrow";
+export { Filter } from "./filter";
+export { CloseLite } from "./closeLite";
--- components/pages/dashboard/search/youtube/index.tsx
+++ components/pages/dashboard/search/youtube/index.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import Modal from 'react-modal';
+import { Filter, CloseLite } from "components/_icons";
 import {
   Pagination,
   Table,
@@ -27,41 +30,89 @@ export default function YoutubeTable() {
   } = useChannels();
   const { countries } = useCountries();
   const { categories } = useCategories();
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
 
   return (
-    <div className={styles.container__element_inner}>
-      <Tab />
-      <div className={styles.filterContainer}>
-        <SearchBox searchText={searchText} setSearchText={setSearchText} />
-        <div className={styles.filterItems}>
-          <Dropdown
-            title="Category"
-            setItem={setCategory}
-            setOffset={setOffset}
-            items={categories}
-            isSearch={true}
-          />
-
-          <Dropdown
-            title="Countries"
-            setItem={setLocation}
-            setOffset={setOffset}
-            items={countries}
-            isSearch={true}
-          />
-        </div>
-
-        <div className={styles.container}>
-          <div className={styles.container__table}>
-            {channels?.length > 0 && <Table items={channels} />}
-            {channels?.length === 0 && <div>No Data Found</div>}
-            {loading && <Loader width="100%" height="100%" />}
+    <>
+      <div className={styles.container__element_inner}>
+        <Tab />
+        <div className={styles.filterContainer}>
+          <div className={styles.search_wrap}>
+            <SearchBox searchText={searchText} setSearchText={setSearchText} />
+            <button onClick={openModal} className={styles.filtersToggle}>
+              <Filter />
+            </button>
           </div>
-          <div className={styles.container__pagination}>
-            <Pagination total={total} setOffset={setOffset} offset={offset} />
+
+          <div className={styles.filterItems}>
+            <Dropdown
+              title="Category"
+              setItem={setCategory}
+              setOffset={setOffset}
+              items={categories}
+              isSearch={true}
+            />
+
+            <Dropdown
+              title="Countries"
+              setItem={setLocation}
+              setOffset={setOffset}
+              items={countries}
+              isSearch={true}
+            />
+          </div>
+
+          <div className={styles.container}>
+            <div className={styles.container__table}>
+              {channels?.length > 0 && <Table items={channels} />}
+              {channels?.length === 0 && <div>No Data Found</div>}
+              {loading && <Loader width="100%" height="100%" />}
+            </div>
+
+            <div className={styles.container__pagination}>
+              <Pagination total={total} setOffset={setOffset} offset={offset} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {/* filter modal */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Filters Modal"
+      >
+        <button onClick={closeModal} className="modal_close_btn"><CloseLite /></button>
+        <div className="edit_filters">
+          <div className={styles.filterItems}>
+            <Dropdown
+              title="Category"
+              setItem={setCategory}
+              setOffset={setOffset}
+              items={categories}
+              isSearch={true}
+            />
+
+            <Dropdown
+              title="Countries"
+              setItem={setLocation}
+              setOffset={setOffset}
+              items={countries}
+              isSearch={true}
+            />
+          </div>
+          <button className="btn_fill_primary">Apply Filter</button>
+        </div>
+      </Modal>
+    </>
   );
 }
