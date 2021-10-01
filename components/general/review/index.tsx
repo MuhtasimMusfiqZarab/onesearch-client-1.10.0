f2d602b5d94f59ecbddd940b04ec6dc1168911a1
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-10-01 16:55:07 +0600

    landing page intro padding fix

--- components/general/review/index.tsx
+++ components/general/review/index.tsx
@@ -1,25 +1,23 @@
-import styles from "./styles.module.scss";
-import Rating from "components/general/rating";
+import Image from 'next/image';
+import styles from './styles.module.scss';
+import Rating from 'components/general/rating';
+import ClientImage from '../../../public/client.png';
 
 export default function Review() {
   return (
     <div className={styles.review}>
       <div className={styles.review__info}>
-        <div className={styles.review__thumb}>
-
-        </div>
+        {/* <div className={styles.review__thumb}> */}
+        <Image src={ClientImage} alt="Search for your lead" width="40" height="40" />
+        {/* </div> */}
         <div className={styles.review__details}>
-          <span className={styles.review__name}>
-            Nafiz Imtiaz
-          </span>
+          <span className={styles.review__name}>Robert</span>
           <span className={styles.review__rating}>
             <Rating rating="4" id="rating" />
           </span>
         </div>
       </div>
-      <p className={styles.review__copy}>
-        Very pleased with their service. Highly recommended!
-      </p>
+      <p className={styles.review__copy}>Very pleased with their service. Highly recommended!</p>
     </div>
   );
 }
