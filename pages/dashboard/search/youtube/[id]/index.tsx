42448aa80480b3db0ebc3271a50aa59cea9abd6e
ashrafuzzaman93 <skshahid.im@gmail.com>
2021-10-19 00:21:44 +0600

    Added save in wishlist button

--- pages/dashboard/search/youtube/[id]/index.tsx
+++ pages/dashboard/search/youtube/[id]/index.tsx
@@ -31,10 +31,10 @@ Channel.BasicInfo = ({ }) => {
   return (
     <>
       <div className={styles.basic__info}>
-        <div className={styles.basic__info_heading}>
-          <h3>Basic information</h3>
-          <a href="" className="brn">Download</a>
-        </div>
+        {/* <div className={styles.basic__info_heading}> */}
+        <h3>Basic information</h3>
+        {/* <a href="" className="brn">Download</a>
+        </div> */}
         <ul>
           <li><span>Channel name:</span> luzysonido hollander </li>
           <li><span>Subscribers:</span> 213,568</li>
@@ -54,10 +54,10 @@ Channel.AnvancedInfo = ({ }) => {
     <div className={styles.advanced__info}>
       <h3>Advanced information</h3>
       <ul>
-        <li><span>Channel name:</span> luzysonido hollander </li>
-        <li><span>Subscribers:</span> 213,568</li>
-        <li><span>Category:</span> Technology </li>
-        <li><span>Views:</span> 1,520,301</li>
+        <li><span>Channel link:</span> luzysonido hollander </li>
+        <li><span>Email address:</span> </li>
+        <li><span>Facebook link:</span> Technology </li>
+        <li><span>Instagram link:</span> www.instagram.com/luizhollander </li>
         <li><span>Location:</span> California, USA </li>
         <li><span>Joining Date:</span> 03/09/2015</li>
         <li><span>Description:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in turpis tempor, bibendum erat vel, dignissim eros. In blandit, arcu in rutrum commodo, mi justo vulputate nulla, vel fringilla augue nulla a lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Donec ut libero quis mi tincidunt tempus.  </li>
