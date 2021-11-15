17720a8cde274408b2704c7fb8c685ebd798ec38
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-15 13:36:48 +0600

    button style change

--- components/pages/home/text/index.tsx
+++ components/pages/home/text/index.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from "react";
-import Link from "next/link";
+import React, { FC } from 'react';
+import Link from 'next/link';
 import Typed from 'react-typed';
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 interface Props {
   title?: string;
   subtitle?: string;
-  typedText?: boolean,
+  typedText?: boolean;
   content: string;
   isDark?: boolean;
   isButton: boolean;
@@ -26,26 +26,22 @@ export const Text: FC<Props> = ({
   buttonUrl,
   buttonClasses
 }: Props): JSX.Element => {
-
   return (
     <div className={`${styles.text} ${isDark ? styles.text__dark : ''}`}>
-      {title && <h2 className={styles.text__title} dangerouslySetInnerHTML={{ __html: title }}></h2>}
-      {
-        typedText && <h2 className={styles.text__title}>
-          <Typed
-            strings={['Search for you lead']}
-            typeSpeed={40}
-            loop
-          />
+      {title && (
+        <h2 className={styles.text__title} dangerouslySetInnerHTML={{ __html: title }}></h2>
+      )}
+      {typedText && (
+        <h2 className={styles.text__title}>
+          <Typed strings={['Search for you lead']} typeSpeed={100} />
         </h2>
-      }
-      {subtitle && <h3 className={styles.text__subtitle} dangerouslySetInnerHTML={{ __html: subtitle }}></h3>}
-      <p
-        className={styles.text__desc}
-        dangerouslySetInnerHTML={{ __html: content }}
-      ></p>
+      )}
+      {subtitle && (
+        <h3 className={styles.text__subtitle} dangerouslySetInnerHTML={{ __html: subtitle }}></h3>
+      )}
+      <p className={styles.text__desc} dangerouslySetInnerHTML={{ __html: content }}></p>
       {isButton && (
-        <Link href={buttonUrl ? buttonUrl : "/"}>
+        <Link href={buttonUrl ? buttonUrl : '/'}>
           <a className={buttonClasses ? buttonClasses : 'btn btn_fill_primary'}>{buttonText}</a>
         </Link>
       )}
