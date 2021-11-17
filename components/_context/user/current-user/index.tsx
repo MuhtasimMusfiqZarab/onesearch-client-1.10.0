2a384fb7d06a75e64191a22b307167f64845a284
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-17 17:58:22 +0600

    add current user context

---
+++ components/_context/user/current-user/index.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from 'react';
+import { useQuery } from '@apollo/client';
+
+import GET_CURRENT_USER from '../../../../pages/api/query/user/get-current-user.gql';
+
+const CurrentUserContext = createContext({
+  currentUser: null
+});
+
+function CurrentUserProvider({ children }) {
+  const { data, error, loading } = useQuery(GET_CURRENT_USER);
+
+  return (
+    <CurrentUserContext.Provider
+      value={{
+        currentUser: data?.currentUser
+      }}>
+      {children}
+    </CurrentUserContext.Provider>
+  );
+}
+
+const useCurrentUser = () => useContext(CurrentUserContext);
+
+export { CurrentUserProvider, useCurrentUser };
