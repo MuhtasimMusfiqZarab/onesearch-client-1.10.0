d572066f2bab023d2d2a57161ddb71650799de40
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-11-20 11:46:36 +0600

    remove vulnarable packages

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: [
-    '../components/**/*.stories.@(ts|tsx|js|jsx)',
-    '../components/**/**/*.stories.@(ts|tsx|js|jsx)',
-    '../components/**/**/**/*.stories.@(ts|tsx|js|jsx)',
-    '../components/**/**/**/**/*.stories.@(ts|tsx|js|jsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
