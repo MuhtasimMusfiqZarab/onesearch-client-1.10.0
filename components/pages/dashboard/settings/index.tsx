1f38fcdc1421637a29fa18b20674108843dfb62b
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-01 22:30:11 +0600

    add profile

--- components/pages/dashboard/settings/index.tsx
+++ components/pages/dashboard/settings/index.tsx
@@ -14,49 +14,53 @@ export default function Settings() {
   return (
     <div className={styles.container}>
       <Tab items={settingsNavElements} />
-      <div className={styles.profile}>
-        <h4>Basic Information</h4>
-        <div className={styles.profile__details}>
-          <div>
-            <span>First Name</span>
-            <span>{currentUser ? currentUser.firstName : <Loader isTextBox />}</span>
-          </div>
-
-          <div>
-            <span>Email: </span>
-            <span>{currentUser ? currentUser.email : <Loader isTextBox />}</span>
-          </div>
-
-          <div>
-            <span>Last Name</span>
-            <span>{currentUser ? currentUser.lastName : <Loader isTextBox />}</span>
-          </div>
-        </div>
-
-        <div className={styles.profile__additional}>
-          <h4>Additional Information</h4>
-          <div className={styles.profile__locators}>
-            <AutofillInput label="Country" />
+      {currentUser ? (
+        <div className={styles.profile}>
+          <h4>Basic Information</h4>
+          <div className={styles.profile__details}>
             <div>
-              <Locator />
+              <span>First Name</span>
+              <span>{currentUser ? currentUser.firstName : <Loader isTextBox />}</span>
+            </div>
+
+            <div>
+              <span>Email: </span>
+              <span>{currentUser ? currentUser.email : <Loader isTextBox />}</span>
+            </div>
+
+            <div>
+              <span>Last Name</span>
+              <span>{currentUser ? currentUser.lastName : <Loader isTextBox />}</span>
             </div>
           </div>
 
-          <span className={styles.profile__warning}>
-            <Warning />
-            &nbsp; *You can set your country just once.
-          </span>
+          <div className={styles.profile__additional}>
+            <h4>Additional Information</h4>
+            <div className={styles.profile__locators}>
+              <AutofillInput label="Country" />
+              <div>
+                <Locator />
+              </div>
+            </div>
 
-          <div className={styles.profile__locators}>
-            <AutofillInput label="Phone Number" />
+            <span className={styles.profile__warning}>
+              <Warning />
+              &nbsp; *You can set your country just once.
+            </span>
+
+            <div className={styles.profile__locators}>
+              <AutofillInput label="Phone Number" />
+            </div>
+
+            <span className={styles.profile__warning}>
+              <Warning />
+              &nbsp; *You can set your contact number just once.
+            </span>
           </div>
-
-          <span className={styles.profile__warning}>
-            <Warning />
-            &nbsp; *You can set your contact number just once.
-          </span>
         </div>
-      </div>
+      ) : (
+        <div>Please login to get access</div>
+      )}
     </div>
   );
 }
