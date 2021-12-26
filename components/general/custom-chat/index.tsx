dc5ce0a476146f9f053979ecf41a74d93eee2f94
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-26 18:14:56 +0600

    clear customer chat file

--- components/general/custom-chat/index.tsx
+++ components/general/custom-chat/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 interface Props {
-  chatSupport: boolean;
+  chatSupport?: boolean;
 }
 
 export const MessengerChat = ({ chatSupport }: Props) => {
   return (
     <FacebookProvider appId="2967314900245282" chatSupport>
-      {chatSupport && <CustomChat pageId="109129984703261" minimized={false} />}
+      <CustomChat pageId="109129984703261" minimized={false} />
     </FacebookProvider>
   );
 };
