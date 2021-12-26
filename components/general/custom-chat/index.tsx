335d722f35c14191bbca210e250d4b6abd7347ae
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-12-26 18:38:12 +0600

    one last chat check

--- components/general/custom-chat/index.tsx
+++ components/general/custom-chat/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
+import styles from './styles.module.scss';
+
 interface Props {
   chatSupport?: boolean;
 }
 
 export const MessengerChat = ({ chatSupport }: Props) => {
   return (
-    <FacebookProvider appId="2967314900245282" chatSupport>
-      <CustomChat pageId="109129984703261" minimized={false} />
-    </FacebookProvider>
+    <div className={chatSupport ? styles.hiddenContainer : ''}>
+      <FacebookProvider appId="2967314900245282" chatSupport>
+        <CustomChat pageId="109129984703261" minimized={false} />
+      </FacebookProvider>
+    </div>
   );
 };
