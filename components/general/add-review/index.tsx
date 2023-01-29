a92e8c5571aceed5d16b5d7d716fdaba8011d363
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-01-29 15:37:26 +0600

    first mutation of review added

--- components/general/add-review/index.tsx
+++ components/general/add-review/index.tsx
@@ -1,8 +1,22 @@
-import Image from 'components/uicontainers/home/services';
+import Image from 'next/image';
 import Rating from 'components/general/rating';
 import styles from './style.module.scss';
 
+import { useQuery, useMutation } from '@apollo/client';
+import ADD_USER_REVIEW from '../../../pages/api/query/user/add-review.gql';
+
 const AddReview = ({ name, avatar, setIsOpen }) => {
+  const [addReview, { data, loading, error }] = useMutation(ADD_USER_REVIEW);
+
+  const setReview = () => {
+    addReview({
+      variables: {
+        input: { id: '5b1aaf11-d6d0-46cd-b71d-bae6b7a1103e', reviewText: 'asdf', rating: 5 }
+      }
+    });
+    setIsOpen(!setIsOpen);
+  };
+
   return (
     <div className={styles.add_review}>
       <h3>Write Review</h3>
@@ -12,13 +26,13 @@ const AddReview = ({ name, avatar, setIsOpen }) => {
       </p>
 
       <div className={styles.add_review__author}>
-        {/* <Image
+        <Image
           src={avatar}
           alt="avatar"
           width={64}
           height={64}
           className={styles.add_review__avatar}
-        /> */}
+        />
         <h4>{name}</h4>
         <Rating rating={0} id="add-review" isEditable={true} />
       </div>
@@ -32,7 +46,7 @@ const AddReview = ({ name, avatar, setIsOpen }) => {
         <button onClick={() => setIsOpen(!setIsOpen)} className="btn btn_fill_primary">
           Skip
         </button>
-        <button onClick={() => setIsOpen(!setIsOpen)} className="btn btn_fill_primary">
+        <button onClick={setReview} className="btn btn_fill_primary">
           Submit
         </button>
       </div>
