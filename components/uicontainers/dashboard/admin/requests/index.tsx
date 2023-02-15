b9c6a4eae6c1c59866e2b9cbfd7cd7d541418b1d
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-02-15 15:14:14 +0600

    request page intermediate change

--- components/uicontainers/dashboard/admin/requests/index.tsx
+++ components/uicontainers/dashboard/admin/requests/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Tab, CsvDrop, ForbiddenAccess } from 'components/general';
+import React, { useState, useEffect } from 'react';
+import Modal from 'components/general/modal';
+import { ForbiddenAccess } from 'components/general';
+import { Filter, CloseLite, Reset } from 'components/_icons';
+import { Pagination, Table, Dropdown, Tab, SearchBox, Loader, Slider } from 'components/general';
+import { UsersTableEnum } from 'components/utils/enum';
 import styles from './styles.module.scss';
 import { useCurrentUser } from 'components/_context/user/current-user';
+import { useAllRequests } from 'components/_context/request/get-requests';
 
 import { adminNavElements } from 'components/utils/resolver/navigation/tab';
 
 export default function Index() {
   const { currentUser, loading: loadingUser } = useCurrentUser();
+  const { getAllRequests, loading: loadingAllUsers } = useAllRequests();
+  let [modalIsOpen, setIsOpen] = useState(false);
+  let [searchText, setSearchText] = useState();
 
   return (
     <>
@@ -15,8 +23,61 @@ export default function Index() {
           <>
             <Tab items={adminNavElements} />
             <div className={styles.filterContainer}>
+              <div className={styles.filterContainer_inner}>
+                <div className={styles.search_wrap}>
+                  <SearchBox searchText={searchText} setSearchText={setSearchText} />
+
+                  <button className={styles.reset_form}>
+                    <Reset />
+                    Reset
+                  </button>
+
+                  <button onClick={() => setIsOpen(true)} className={styles.filtersToggle}>
+                    <Filter />
+                  </button>
+                </div>
+
+                {/* <div className={styles.filterItems}>
+                  <Dropdown
+                    title="Category"
+                    setItem={setCategory}
+                    setOffset={setOffset}
+                    items={categories}
+                    isSearch={true}
+                  />
+
+                  <Dropdown
+                    title="Countries"
+                    setItem={setLocation}
+                    setOffset={setOffset}
+                    items={countries}
+                    isSearch={true}
+                  />
+                </div> */}
+                {/* <div className={styles.filterItems}>
+                  <Slider />
+                </div> */}
+              </div>
+
               <div className={styles.container}>
-                <CsvDrop />
+                <div className={styles.container__table}>
+                  {getAllRequests?.users?.length > 0 && (
+                    <Table
+                      items={getAllRequests?.requests}
+                      headersEnums={UsersTableEnum}
+                      hasController={false}
+                      hasCheckbox={false}
+                    />
+                  )}
+                  {loadingAllUsers && (
+                    <Table items={getAllRequests?.requests} headersEnums={UsersTableEnum} loading />
+                  )}
+                  {getAllRequests?.requests?.length === 0 && <div>No User Data Found</div>}
+                </div>
+
+                {/* <div className={styles.container__pagination}>
+                  <Pagination total={total} setOffset={setOffset} offset={offset} />
+                </div> */}
               </div>
             </div>
           </>
@@ -24,6 +85,61 @@ export default function Index() {
           <ForbiddenAccess />
         )}
       </div>
+
+      {/* filter modal */}
+      <Modal isOpen={modalIsOpen} setIsOpen={setIsOpen}>
+        <div className={styles.edit_filters}>
+          <div className={styles.search_wrap}>
+            {/* <SearchBox searchText={searchText} setSearchText={setSearchText} /> */}
+            <button className={`${styles.reset_form} ${styles.reset_form_modal}`}>
+              <Reset />
+            </button>
+          </div>
+          <h3>Edit Filters</h3>
+          {/* <div className={styles.filterItems}>
+            <Dropdown
+              title="Category"
+              setItem={setCategory}
+              setOffset={setOffset}
+              items={categories}
+              isSearch={true}
+              isInline={true}
+            />
+
+            <Dropdown
+              title="Countries"
+              setItem={setLocation}
+              setOffset={setOffset}
+              items={countries}
+              isSearch={true}
+              isInline={true}
+            />
+
+            <Dropdown
+              title="Year"
+              setItem={setLocation}
+              setOffset={setOffset}
+              items={countries}
+              isSearch={true}
+              isInline={true}
+            />
+
+            <Dropdown
+              title="Subscriber"
+              setItem={setLocation}
+              setOffset={setOffset}
+              items={countries}
+              isSearch={true}
+              isInline={true}
+            />
+          </div> */}
+          <div className="text-center">
+            <button type="submit" className="btn_fill_secondary">
+              Apply Filter
+            </button>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
