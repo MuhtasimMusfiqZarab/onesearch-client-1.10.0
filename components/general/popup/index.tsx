1a96967ed79f4349d901ba12f4cd39d0590a8ca2
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-02-28 16:08:43 +0600

    create a popup box

---
+++ components/general/popup/index.tsx
@@ -0,0 +1,62 @@
+import React, { FC, useState, useEffect } from 'react';
+import styles from './style.module.scss';
+import { ArrowLeft, ArrowRight } from 'components/_icons';
+
+interface Props {
+  offset: number;
+  setOffset: (value: number) => void;
+  total: any;
+}
+
+export const Popup: FC<Props> = ({}): JSX.Element => {
+  const [isShown, setIsShown] = useState<boolean>(false);
+  const [isClosed, setIsClosed] = useState<boolean>(true);
+
+  const goToFirst = () => {};
+
+  const goToLast = () => {};
+
+  return (
+    <div>
+      {!isShown ? (
+        <button id="delete" className={styles.btn_primary} onClick={() => setIsShown(!isShown)}>
+          <svg viewBox="0 0 448 512" width="100">
+            <path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z" />
+          </svg>
+          Delete
+        </button>
+      ) : (
+        <button
+          id="delete"
+          className={`${styles.btn_primary} ${styles.notification_show}`}
+          onClick={() => setIsShown(!isShown)}>
+          <svg viewBox="0 0 448 512" width="100">
+            <path d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z" />
+          </svg>
+          Delete
+        </button>
+      )}
+
+      <div id="notification" className={styles.notification}>
+        <svg className={styles.notification__warning} viewBox="0 0 512 512" width="100">
+          <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z" />
+        </svg>
+        <p>Item has been deleted</p>
+        <button className={styles.btn_secondary}>Undo</button>
+        {isClosed ? (
+          <button id="close" className={styles.notification__close}>
+            <svg viewBox="0 0 352 512" width="100">
+              <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+            </svg>
+          </button>
+        ) : (
+          <button id="close" className={styles.notification__close}>
+            <svg viewBox="0 0 352 512" width="100">
+              <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+            </svg>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
