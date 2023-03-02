04f43c4d8f323038d37495566d423858af4f5e64
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-03-02 18:39:07 +0600

    add titles

--- components/_context/linkedin/titles/index.tsx
+++ components/_context/linkedin/titles/index.tsx
@@ -1,14 +1,14 @@
 import React, { createContext, useContext } from 'react';
 import { useQuery } from '@apollo/client';
 
-import GET_CHANNEL_COUNTRIES from '../../../../pages/api/query/youtube/get-channel-countries.query.gql';
+import GET_LINKEDIN_TITLES from '../../../../pages/api/query/linkedin/get-linkedin-titles.gql';
 
 const LinkedinCompaniesContext = createContext({
   titles: null
 });
 
 function LinkedinTitlesProvider({ children }) {
-  const { data, error, loading } = useQuery(GET_CHANNEL_COUNTRIES);
+  const { data, error, loading } = useQuery(GET_LINKEDIN_TITLES);
 
   return (
     <LinkedinCompaniesContext.Provider
