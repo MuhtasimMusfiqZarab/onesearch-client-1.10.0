706f2a4b62149986d17c398525176a4b57aaf9ab
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2023-03-10 17:10:59 +0600

    fix issue

--- components/uicontainers/404/index.tsx
+++ components/uicontainers/404/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { NotFoundIcon } from "components/_icons";
+import React, { FC } from 'react';
+import { NotFoundIcon } from 'components/_icons';
 
 interface Props {}
 
@@ -7,7 +7,7 @@ const Index: FC<Props> = ({}: Props): JSX.Element => {
   return (
     <div>
       <NotFoundIcon />
-      <div>Page Not Found!</div>
+      <div>Page Not Found!!</div>
     </div>
   );
 };
